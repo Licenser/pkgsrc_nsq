4d66fb3ea29cd06c8291e9909c9f4e9afe8b3033
Heinz N. Gies <heinz@licenser.net>
2015-05-21 19:26:39 +0200

    Added smf inport.

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -4,5 +4,9 @@ case $2 in
     PRE-INSTALL)
         ;;
     POST-INSTALL)
+        for manifest in nsqadmin.xml nsqd.xml nsqlookupd.xml
+        do
+	        svccfg import /opt/local/lib/svc/manifest/$manifest
+        done
         ;;
 esac
